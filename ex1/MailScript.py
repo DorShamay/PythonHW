0a7768b2cf3e2c03a30db6b4c800a1a0d0e5b02a
DorShamay <dor.shamay.1@gmail.com>
2018-06-12 22:44:53 +0300

    Added some new features

--- ex1/MailScript.py
+++ ex1/MailScript.py
@@ -2,13 +2,14 @@
 
 #########################################
 # Porpuse : Send Mail                   #
-# Version : 0.0.1                       #
+# Version : 0.0.2                       #
 # Creator : DorShamay                   #
 #########################################
 
 import smtplib
 import email.utils
 import sys
+import getpass
 
 from email.mime.text import MIMEText
 
@@ -34,17 +35,16 @@ def mainmenu():
 def main():
     try:
         gmailuser = input("Write your Gmail User here --> ")
-        password = input("Write your Gmail password here --> ")
+        password = getpass.getpass("Write your Gmail password here --> ")
         recipient = input("Please write your Recipient Mail --> ")
         author = input("Please write your email address --> ")
-        description = input("Put your description here")
-        subject = input("Please put your subject here")
-
+        subject = input("Please put your subject here --> ")
+        description = input("Put your description here --> ")
 
     # Create the message
         msg = MIMEText(description)
-        msg['To'] = email.utils.formataddr(('Recipient', recipient))
-        msg['From'] = email.utils.formataddr(('Author', author))
+        msg['To'] = email.utils.formataddr((recipient, recipient))
+        msg['From'] = email.utils.formataddr((author, author))
         msg['Subject'] = subject
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -55,8 +55,11 @@ def main():
         server.set_debuglevel(False) # show communication with the server
         try:
             server.sendmail(author, recipient, msg.as_string())
+            sys.exit(0)
         finally:
-            server.quit()
+            pass
+    except TypeError:
+        print("You didn't write the input correctly please try again")
     except KeyboardInterrupt:
         print(exit)
         mainmenu()
